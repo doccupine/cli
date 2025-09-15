daf18687d3242b801b5d4d7f69bf1547f44b522d
Luan Gjokaj <luan@riangle.com>
2025-09-15 11:52:40 +0200

    feat: update version number

--- src/index.ts
+++ src/index.ts
@@ -593,7 +593,7 @@ program
   .description(
     "Watch MDX files and generate Next.js documentation pages automatically",
   )
-  .version("0.0.6");
+  .version("0.0.7");
 
 program
   .command("watch", { isDefault: true })
