cbc03b6f67ca1c666a7f1e7cc3785eec58b3d0a9
Luan Gjokaj <luan@riangle.com>
2026-07-30 01:58:37 +0200

    chore(templates): bump posthog-js in the generated app
    Update the generated app's posthog-js dependency from ^1.407.8 to
    ^1.408.0.

--- src/templates/package.ts
+++ src/templates/package.ts
@@ -26,7 +26,7 @@ export const packageJsonTemplate =
         minisearch: "^7.2.0",
         next: "16.2.12",
         "next-mdx-remote": "^6.0.0",
-        "posthog-js": "^1.407.8",
+        "posthog-js": "^1.408.0",
         "posthog-node": "^5.46.1",
         react: "19.2.8",
         "react-dom": "19.2.8",
